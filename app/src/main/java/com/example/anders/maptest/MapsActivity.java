639901586852ce4cc9c9bc0c8e5ed63fb060a44b
AndersNKnudsen <anders.nordli.knudsen@gmail.com>
2016-12-17 00:42:09 +0100

    Added network markers for location testing, added new photo made by phone camera of PC screen, then resized and added on google map view.

--- app/src/main/java/com/example/anders/maptest/MapsActivity.java
+++ app/src/main/java/com/example/anders/maptest/MapsActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,10 +22,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.content.pm.PackageManager;
 import android.Manifest;
-import com.google.android.gms.common.api.GoogleApiClient.*;
-import com.google.android.gms.location.LocationServices;
-import android.location.Location;
-import java.security.KeyStore;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -67,11 +62,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng oslo = new LatLng(59.911491, 10.757933);
-        mMap.addMarker(new MarkerOptions().position(oslo).title("Oslo"));
+        //LatLng oslo = new LatLng(59.911491, 10.757933);
+        //mMap.addMarker(new MarkerOptions().position(oslo).title("Oslo"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(oslo, 10.0f));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(oslo));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(oslo));
         //mMap.animateCamera(CameraUpdateFactory.zoomIn());
         Log.d("---TEST---", "onMapReady: Checking mylocation2");
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
@@ -79,8 +73,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d("---TEST---", "onMapReady: Setting mylocation");
             mMap.setMyLocationEnabled(true);
             try {
-                LatLng self = new LatLng(getLocation().getLatitude(), getLocation().getLongitude());
-                myMarker = mMap.addMarker(new MarkerOptions().position(self).title("Start 1"));
+                //LatLng self = new LatLng(getLocation().getLatitude(), getLocation().getLongitude());
+                //myMarker = mMap.addMarker(new MarkerOptions().position(self).title("Start 1"));
+                Location selfGPS = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+               // Location selfNet = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                if(selfGPS != null)
+                    myMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(selfGPS.getLatitude(), selfGPS.getLongitude())).title("GPS Location"));
+                /*if(selfNet != null)
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(selfNet.getLatitude(), selfNet.getLongitude())).title("Network Location"));*/
             }
             catch(NullPointerException npe)
             {
@@ -95,11 +96,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Add test map
         BitmapDescriptor im = BitmapDescriptorFactory.fromResource(R.drawable.karttest2);
 
-        LatLngBounds bounds = new LatLngBounds(new LatLng(59.970068, 10.724203), new LatLng(59.984807, 10.761013));
+        LatLng llSW = new LatLng(59.970068, 10.724203);
+        LatLng llNE = new LatLng(59.984807, 10.761013);
 
-        GroundOverlay groundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+        LatLngBounds bounds = new LatLngBounds(llSW, llNE);
+
+        GroundOverlay goOrientering = mMap.addGroundOverlay(new GroundOverlayOptions()
                 .image(im)
                 .positionFromBounds(bounds)
+                .transparency(0.5f));
+
+        LatLng llMapZoom = new LatLng((llNE.latitude + llSW.latitude) / 2, (llNE.longitude + llSW.longitude) / 2);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(llMapZoom, 13.0f));
+
+        //Add secondary groundoverlay map from camera photo of PC... >_>
+        BitmapDescriptor imCam = BitmapDescriptorFactory.fromResource(R.drawable.camorient);
+
+        LatLng llSWCam = new LatLng(59.96446, 10.59739);
+        LatLng llNECam = new LatLng(59.97900, 10.63691);
+
+        LatLngBounds bounds2 = new LatLngBounds(llSWCam, llNECam);
+
+        GroundOverlay goOrient2 = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(imCam)
+                .positionFromBounds(bounds2)
                 .transparency(0.5f));
 
     }
@@ -107,7 +128,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onResume() {
         super.onResume();
-        try{
+        onLocationChanged(getLocation());
+        /*try{
             //return locMan.getLastKnownLocation(provider);
             onLocationChanged(locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER));
 
@@ -119,15 +141,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         catch (SecurityException e)
         {
             Log.d("---TEST---", "onMapReady: Could not get location due to permissions. Check.");
-        }
+        }*/
     }
 
     public Location getLocation()
     {
+        Location result;
         try{
             //return locMan.getLastKnownLocation(provider);
             return locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
+            //result = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         }
         catch (NullPointerException npe)
         {
@@ -137,23 +160,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         {
             Log.d("---TEST---", "onMapReady: Could not get location due to permissions.");
         }
-        Location failed = new Location("");
-        failed.setLongitude(0.0f);
-        failed.setLatitude((0.0f));
-        return failed;
+        result = new Location("");
+        result.setLongitude(0.0f);
+        result.setLatitude((0.0f));
+        return result;
     }
 
     @Override
     public void onLocationChanged(Location location) {
 
-        myMarker.remove();
+        if(myMarker != null)
+            myMarker.remove();
         try {
             LatLng self = new LatLng(getLocation().getLatitude(), getLocation().getLongitude());
-            myMarker = mMap.addMarker(new MarkerOptions().position(self).title("2 Start"));
+            myMarker = mMap.addMarker(new MarkerOptions().position(self).title("GPS Loc2"));
+
+            Location selfNet = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if(selfNet != null)
+                mMap.addMarker(new MarkerOptions().position(new LatLng(selfNet.getLatitude(), selfNet.getLongitude())).title("Network Location"));
+
         }
         catch(NullPointerException npe)
         {
             Log.d("---TEST---", "Could not create mymarker position.");
+        }
+        catch (SecurityException e)
+        {
+            Log.d("---TEST---", "onMapReady: Could not get location due to permissions.");
         }
     }
 
